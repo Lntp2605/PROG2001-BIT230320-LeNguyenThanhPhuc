16be74ab380bdafee3b042173ae2054e94d6d930
Lntp2605 <hhhg09171@gmail.com>
2026-04-13 13:21:30 +0700

    lol

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <stdio.h>
 
 int main() {
 
-
     printf("Chào mừng bạn đến với lập trình C!\n");
     printf("Bắt đầu hành trình của bạn ngay hôm nay\n");
+
+    float pi = 3.14159;
+    printf("%f\n", pi);
+    printf("%.1f\n", pi);
+    printf("%.4f\n", pi);
+
+    int a = 2;
+    float b = 3.14;
+    char c = 'a';
+    bool d = true;
+    printf("%d, %.2f, %c, %d\n", a, b, c, d);
+
+
+
 
     return 0;
 }
